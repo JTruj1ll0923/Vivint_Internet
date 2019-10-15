e55ee857879d87053e62ed0c7e80198c0a1d3c44
JTruj1ll0923 <jtrujillo923@gmail.com>
2019-10-15 16:13:59 -0600

    Cleanup and give more easy to read info

--- speedtest.sh
+++ speedtest.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-#mkdir ~/speedtest-cli
-date
 wget -P ~/speedtest-cli https://raw.githubusercontent.com/JTruj1ll0923/Vivint_Internet/master/speedtest.tar.gz
 wait
-date
-tar -xvzf ~/speedtest-cli/speedtest.tar.gz -C ~/speedtest-cli/
+tar -xzf ~/speedtest-cli/speedtest.tar.gz -C ~/speedtest-cli/
 chmod +x ~/speedtest-cli/speedtest/speedtest
+echo ""
+echo ""
+echo ""
+echo "About to run a speedtest!"
+echo "--------------------------------------"
 ~/speedtest-cli/speedtest/speedtest
+echo "--------------------------------------"
+echo "Done!"
+echo ""
+echo ""
+echo ""
 rm -rf ~/speedtest-cli
